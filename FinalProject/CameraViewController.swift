da5deaa8a4d404f986d602a9203c0db35278ed76
Nikethana-Nagasubramanian <nikethanan01@gmail.com>
2023-11-28 16:12:35 -0500

    Text-To-Speech

--- FinalProject/CameraViewController.swift
+++ FinalProject/CameraViewController.swift
@@ -6,24 +6,100 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
 
-class CameraViewController: UIViewController {
-
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let synthesizer = AVSpeechSynthesizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        do{
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+         }
+        catch
+        { print("Fail to enable session") }
+    }
+    var image: UIImage?
+    var recognizedText: String = ""
+    
+    func presentImagePicker() {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("Camera is not available on this device.")
+            return
+        }
 
-        // Do any additional setup after loading the view.
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        present(picker, animated: true)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        picker.dismiss(animated: true)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let uiImage = info[.originalImage] as? UIImage {
+            processImage(uiImage)
+        }
     }
-    */
+    
+    private func processImage(_ image: UIImage) {
+//        guard let cgImage = image.cgImage else { return }
+//
+//        let request = VNRecognizeTextRequest { [weak self] (request, error) in
+//            guard let self = self else { return }
+//
+//            if let error = error {
+//                print("Error: \(error)")
+//                return
+//            }
+//
+//            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+//            self.recognizedText = observations.compactMap { $0.topCandidates(1).first?.string }.joined(separator: " ")
+//        }
+//
+//        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+//        try? requestHandler.perform([request])
+        guard let cgImage = image.cgImage else { return }
+
+        let request = VNRecognizeTextRequest { [weak self] (request, error) in
+            guard let self = self else { return }
+
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            let recognizedStrings = observations.compactMap { observation -> String? in
+                guard let candidate = observation.topCandidates(1).first else { return nil }
+                return candidate.confidence > 0.8 ? candidate.string : nil // Adjust confidence threshold as needed
+            }.joined(separator: " ")
+
+            DispatchQueue.main.async {
+                self.recognizedText = recognizedStrings
+            }
+        }
+
+        request.recognitionLevel = .accurate // Set recognition level to accurate for better results
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+        try? requestHandler.perform([request])
+    }
+
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.rate = 0.5
+        synthesizer.speak(utterance)
+    }
+
+    @IBAction func btnClicked(_ sender: UIButton) {
+        print("Recognized text : \(recognizedText)")
+        speak(text: recognizedText)
+    }
+    @IBAction func imgBtnClicked(_ sender: UIButton) {
+        presentImagePicker()
+    }
 
 }
--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    let synthesizer = AVSpeechSynthesizer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        do{
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+         }
+        catch
+        { print("Fail to enable session") }
     }
 
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US") // Choose the desired language
+        utterance.rate = 0.5 // Adjust the speech rate as needed
+        synthesizer.speak(utterance)
+    }
 
+    @IBAction func btnClicked(_ sender: UIButton) {
+        let textToSpeak = "Hello, World! This is a test." // Replace with your desired text
+        speak(text: textToSpeak)
+    }
+    func recognizeText(image: UIImage, completion: @escaping (String?) -> Void) {
+        guard let cgImage = image.cgImage else {
+            completion(nil)
+            return
+        }
+
+        let textRecognitionRequest = VNRecognizeTextRequest { request, error in
+            if let error = error {
+                print("Error recognizing text: \(error)")
+                completion(nil)
+                return
+            }
+
+            guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                completion(nil)
+                return
+            }
+
+            let recognizedText = observations.compactMap { observation in
+                observation.topCandidates(1).first?.string
+            }.joined(separator: " ")
+
+            completion(recognizedText)
+        }
+
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+        try? requestHandler.perform([textRecognitionRequest])
+    }
 }
 
